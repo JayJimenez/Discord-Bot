05b1763114f516a4ffbc2ea5d007c6077bfcc1a1
Jay Jimenez <jayjimenez.nm@gmail.com>
2020-09-12 00:34:10 -0600

    Deleted token from file - whoops...

--- sml-bot.py
+++ sml-bot.py
@@ -3,11 +3,7 @@ from discord.ext import commands
 from congress import Congress
 
 
-
-TOKEN = "NzUzMTI0OTg0NDk4NTUyODg0.X1hoTA.sPwcofaclgYczyT2n2l3dqW_Yik"
-
-
-
+TOKEN = ""
 
 bot = commands.Bot(command_prefix='>')  
 
